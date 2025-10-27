80bffe180c043a71f651e353616160eaa95e405e
Antoine <antoine.milhau@hotmail.fr>
2025-10-27 11:18:05 +0100

    Initial commit

---
+++ src/test/java/fr/antoine/steamrest/SteamRestApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.antoine.steamrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
